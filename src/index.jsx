edcc8c8bfda9fb1fccc68d7a01e45f2f74086e63
Manuel Mendoza <joseguns2@gmail.com>
2020-11-27 16:18:10 -0500

    Genesis

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const container = document.getElementById('root')
+
+ReactDOM.render( 
+    <h1>
+        Hola Mundo
+    </h1>,container)
