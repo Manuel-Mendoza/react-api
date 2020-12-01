3a4be07d8a78fe41337857ebef6b0b25fca64921
Manuel Mendoza <joseguns2@gmail.com>
2020-12-01 04:12:29 -0500

    Por fin Pude

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
 const container = document.getElementById('root')
+const api = 'https://api.opendota.com/api/';
 
-ReactDOM.render( 
-    <h1>
-        Hola Mundo
-    </h1>,container)
+class API extends Component {
+    state = {
+        data: [],
+        isFetch: true
+    }
+
+    componentDidMount() {
+        /* Consulta al ID */
+        fetch(api + 'players/338324299')
+            .then(response => response.json())
+            .then(info => this.setState({data:info,isFetch:false}))
+    }
+
+    render() {
+        /* Montando información */
+        if (this.state.isFetch) {
+            return 'Loading...'
+        }
+        
+        const estado = this.state.data
+        return(
+            <div>
+                <p>Nombre: {estado.profile.personaname} </p>
+                <p>Pais: {estado.profile.loccountrycode} </p>
+                <p>Imagen <img src={estado.profile.avatarfull} alt=""/></p>
+                <a href={estado.profile.profileurl} target='blank'>Cuenta de Steam: {estado.profile.personaname}</a>
+            </div>
+        )
+
+    }
+}
+
+ReactDOM.render(
+    <div>
+        <API />
+        <small>Información sacada de OpenDota</small>
+    </div>, container)
